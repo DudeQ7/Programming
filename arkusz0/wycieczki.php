ebe6dc8ea2875b917b513ec85d0fe7e6afdbba4b
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-09-25 10:49:01 +0200

    CIEKAWOSC

---
+++ arkusz0/wycieczki.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wycieczki i urlopy</title>
+    <link rel="stysheet"href="styl3.css">
+</head>
+<body>
+<section class="baner">
+    <h1>Biuro Podróży</h1>
+</section>
+<section class="lewy">
+    <h2>Kontakt</h2>
+</section>
+<section class="srodkowy">
+    <h2>Galeria</h2>
+  <p>  LOL SKRYPT HOGWARTU</p>
+</section>
+<section class="prawy">
+    <h2>PROMOCJE</h2>
+    <table >
+        <tr>Jesień</tr>
+        <tr>Grupa 4+</tr>
+        <tr>Grupa 10+</tr>
+        <td>5%</td>
+        <td>10%</td>
+        <td>15%</td>
+    </table>
+</section>
+<section class="dane"></section>
+<section class="stopka"></section>
+
+</body>
+<?php 
+?>
