7f99193a465a9d6ca6d97d54fcd02b1e68edcf4b
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-09-27 11:06:29 +0200

    WORKING :)

--- arkusz0/wycieczki.php
+++ arkusz0/wycieczki.php
@@ -12,10 +12,26 @@
 </section>
 <section class="lewy">
     <h2>Kontakt</h2>
+    <a href="mailto:kontakt@gmail.com" > Napisz do nas</a>
+    <p>telefon 555666777</p>
 </section>
 <section class="srodkowy">
     <h2>Galeria</h2>
-  <h2>LOL SKRYPT HOGWARTU</h2>
+  <h2><?php
+$user = 'root';
+$ip = 'localhost';
+$password = '';
+$dbname = 'db4';
+$query = "Select nazwaPliku,podpis from zdjecia order by(podpis) ASC";
+$conn= mysqli_connect($ip,$user,$password,$dbname);
+$result = mysqli_query($conn,$query);
+//Select nazwaPliku,podpis from zdjecia order by(podpis) ASC;
+while($row = mysqli_fetch_array($result)) {
+    echo  "<img src='$row[0]' alt='$row[1]' />" ;
+
+}
+mysqli_close($conn);
+?></h2>
 </section>
 <section class="prawy">
     <h2>PROMOCJE</h2>
@@ -33,28 +49,21 @@
 </table>
 
 </section>
-<section class="dane"></section>
+<section class="dane">
+<h2>LISTA WYCIECZEK</h2>
+<?php
+$conn1 = mysqli_connect($ip,$user,$password,$dbname);
+$query1 = "SELECT id,dataWyjazdu,cel,cena from wycieczki WHERE dostepna = 1 ";
+$result1 = mysqli_query($conn1,$query1);
+while ($row1 = mysqli_fetch_array($result1)){
+ echo $row1['id'] . "  " .$row1['dataWyjazdu']. "  " . $row1['cel'] . "  ". "cena: " . $row1['cena'] . "</br>" ;
+}
+mysqli_close($conn1);
+
+?>
+</section>
 <section class="stopka">
     <p>Strone wykonal: 01010 </p>
 </section>
 
 </body>
-<?php
-$user = 'root';
-$ip = 'localhost';
-$password = '';
-$dbname = 'db4';
-$query = "Select nazwaPliku,podpis from zdjecia order by(podpis) ASC";
-$conn= mysqli_connect($ip,$user,$password,$dbname);
-$result = mysqli_query($conn,$query);
-//Select nazwaPliku,podpis from zdjecia order by(podpis) ASC;
-
-while($row = mysqli_fetch_assoc($result)) {
-    $zmienna_potegi = 0;
-    echo "<img src=" .$row['nazwaPliku'] .". alt=" . $row['podpis']. "";
-
-}
-mysqli_close($conn);
-
-
-?>
