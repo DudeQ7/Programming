81e05911c83e05cc2079725c528b0e02b47b451c
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-10-02 11:24:12 +0200

    Popraw to please :)

--- arkusz1/opinie.php
+++ arkusz1/opinie.php
@@ -1,43 +1,50 @@
-<!DOCTYPE html5>
-<html lang="pl">
-<head>
-<meta charset="utf-8">
-<title>Opinie klientów</title>
-<link rel="stylesheet"href="styl3.css">
-</head>
-<body>
-<section class="baner_1">
-    <h1>Hurtownia spożywcza</h1>
-</section>
-<section class="glowny_2">
-    <h2>Opinie naszych klientów</h2>
-    SKRYPT 1 
-</section>
-<section class="stopka_3">
-    <h3>Współpracują z nami</h3>
-    <a href="http://sklep.pl">Sklep 1</a>
-</section>
-<section class="stopka_4">
-    <h3>Nasi top klienci </h3>
-    <ul>
-    <li>SKRYPT 2 </li>
-    </ul>
-</section>
-<section class="stopka_5">
-    <h3>Skontaktuj sie</h3>
-    <p>telefon:111222333</p>
-</section>
-<section class="stopka_6">
-    <h3>Autor: Ja muhahah </h3>
-</section>
-
-</body>
 <?php
 $user = "root";
 $password = "";
 $db = "db7";
 $ip = "localhost";
-$conn = mysqli_connect($ip,$user,$password,$db,$ip);
-$query = "";
-mysqli_close($conn);
+$conn = mysqli_connect($ip, $user, $password, $db);
+$query = "SELECT zdjecie, imie, opinia FROM klienci INNER JOIN opinie ON klienci.id = opinie.Klienci_id WHERE Typy_id = 2 or Typy_id = 3;";
+$data = mysqli_query($conn, $query);
 ?>
+
+<!DOCTYPE html5>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Opinie klientów</title>
+    <link rel="stylesheet" href="styl3.css">
+</head>
+<body>
+    <section class="baner_1">
+        <h1>Hurtownia spożywcza</h1>
+    </section>
+    <section class="glowny_2">
+        <h2>Opinie naszych klientów</h2>
+        <?php
+        while ($build = mysqli_fetch_array($data)) {
+            echo 'img src=."$build['zdjecie']".alt = "klienci>';
+            echo $build["opinia"];
+        }
+        ?>
+
+    </section>
+    <section class="stopka_3">
+        <h3>Współpracują z nami</h3>
+        <a href="http://sklep.pl">Sklep 1</a>
+    </section>
+    <section class="stopka_4">
+        <h3>Nasi top klienci </h3>
+        <ul>
+            <li>SKRYPT 2 </li>
+        </ul>
+    </section>
+    <section class="stopka_5">
+        <h3>Skontaktuj się</h3>
+        <p>telefon: 111222333</p>
+    </section>
+    <section class="stopka_6">
+        <h3>Autor: Ja muhahah</h3>
+    </section>
+</body>
+</html>
