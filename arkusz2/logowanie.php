af63cdf1955cb6bd8ab3f234ba7db777ec688833
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-10-06 15:02:59 +0200

    com1

---
+++ arkusz2/logowanie.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <link rel="stylesheet" href="styl4.css">
+    <section class="baner">
+    <h1>Forum wielbicieli psow</h1>
+    </section>
+    <section class="lewy">
+        <img src="obraz.jpg" alt="foksterier">
+    </section>
+    <section class="prawy_1">
+        <h2>Zapisz sie</h2>
+        <form>
+            <input type="login" name="login">login:</input>
+            <input type="password"  name="password">haslo: </input>
+            <input type="password" name="password2">powtorz haslo: </input>
+            <input type="submit" name="Zapisz" ></input>
+        </form>
+        SKRYPT HOGWARTU
+    </section> </br>
+    <section class="prawy_2"></section>
+    <section class="stopka"></section>
+</body>
+</html>
