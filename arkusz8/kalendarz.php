3ef9efd79eb484303953694da0d13178018c386e
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-11-06 10:19:51 +0100

    Baba_G

--- arkusz8/kalendarz.php
+++ arkusz8/kalendarz.php
@@ -36,25 +36,32 @@ $golf5 = mysqli_connect($ip,$user,$passwd,$db_name);
         $query_2 = "SELECT dataZadania,wpis FROM zadania WHERE miesiac = 'lipiec';";
         $porsche_GT3_RS = mysqli_query($golf5_2, $query_2);
         $lolik = 'dni';
-        //  echo "<section class='$zmienna_testowa'>";
-        echo "<section class='$lolik'>"; 
-        while ($BMWM5 = mysqli_fetch_array($porsche_GT3_RS)){
-            echo "<h5>".$BMWM5['dataZadania']."</h5>";
-            echo "<p>".$BMWM5['wpis']."</p>";
+        while ($wynik = mysqli_fetch_array($porsche_GT3_RS)) {
+            echo "<section class='$lolik'><h5>$wynik[0]</h5><p>$wynik[1]</p></section>";
         }
-        echo "</section>";
         mysqli_close($golf5_2);
         ?>    
-        
-      
         </section>
        
      
         <section class="stopka">
             <form method="post">
                Dodaj wpis: <input type="text" name="wpis">
-               Dodaj<input type="submit" name="DODAJ">
+               Dodaj<input type="submit" name="wyslij">
                 <p> Stronę wykonał: GOLF mk7 GTI </p>
             </form>
+            <?php 
+         
+           if(isset($_POST["wyslij"])) {
+            $wpis =  $_POST['wpis'];
+            //zapytanie 4 = UPDATE zadania SET wpis = "Wycieczka:jezioro" WHERE dataZadania = '2020-07-13' ;
+            $golf5_3 =mysqli_connect($ip,$user,$passwd,$db_name); 
+            $query4 = "UPDATE zadania set wpis = '$wpis' WHERE dataZadania = '2020-07-13' ; ";
+            $hogwart =  mysqli_query($golf5_3,$query4);
+             echo "Nazwa zmieniona na:". $wpis ;
+           
+           }
+            ?>
+
     </section>
     </body>
