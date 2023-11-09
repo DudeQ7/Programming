d556b50e4c5429cf77a94245d6757c52c7654c88
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-11-09 22:52:54 +0100

    Baba_G

--- arkusz10/airport.php
+++ arkusz10/airport.php
@@ -2,7 +2,8 @@
 $ip = 'localhost';
 $user = 'root';
 $password = '';
-$db_name = 'db18';
+// $db_name = 'db18'; nazwa bazy na lapku
+$db_name = 'db15'; // nazwa bazy na stacjonarce 
 $conn = mysqli_connect($ip, $user, $password, $db_name);
 
 ?>
@@ -26,27 +27,49 @@ $conn = mysqli_connect($ip, $user, $password, $db_name);
         <h4> Tabela odlotów </h4>
             <table>
                 <?php
+                echo "
+                        <th>lp</th>
+                        <th>numer rejsu</th>
+                        <th>czas</th>
+                        <th>kierunek</th>
+                        <th>status</th>
+                    ";
                 $query_1 = 'SELECT id,nr_rejsu,czas,kierunek,status_lotu FROM odloty ORDER BY czas DESC;';
                 $hogwart = mysqli_query($conn,$query_1);
-                    echo "<table";
-                    while ($row = mysqli_fetch_array($hogwart))
-                    {
-                echo "
-                    <td><th>lp.</th></td>
-                    <td><th>NUMER REJSU</th></td>
-                    <td><th>CZAS</th></td>
-                    <td><th>KIERUNEK</th></td>
-                    <td><th>STATUS</th></td>";
+                while ($row = mysqli_fetch_array($hogwart)) {
+                    echo "<tr>";
+                    for($lego = 0; $lego < 5; $lego++) {
+                        echo "
+                                <td>$row[$lego] </td> 
+                        "; //pokazuje elementy wyświetla 5 elementów z bazy danych ;) 
+                    }
+                    echo "</tr>";
                 }
-           echo  "</table>";
-            
+                mysqli_close($conn);
             ?>
     </section>
     <section class="footer_1">
         <a href="kw1.jpg"Download target="_blank">Pobierz obraz </a>
     </section>
     <section class="footer_2">
-        SKRYPT 2 
+    <?php
+$conn_2 = mysqli_connect($ip, $user, $password, $db_name);
+if (isset($_COOKIE["ciasteczko"])) {
+    echo "<p><i>Dzień dobry! Sprawdź regulamin naszej strony</i></p>";
+} else {
+    $cookie_name = "cookie";
+    $cookie_value = "";
+    $cookie_time = time() + 60*60*2;
+    $cookie_path = "";
+    $cookie_domain = "";
+    $cookie_secure  = false;
+    $cookie_httponly = false;
+    echo "<p><b>Miło nam, że nas znowu odwiedziłeś</b></p>";
+}
+
+?>
+
+
     </section>
     <section class="footer_3">
         Autor: Golf Mk7 GTI 
