c6ed0806f1fed400c82f4b0bd097e821dcd9f6e8
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-11-13 23:27:28 +0100

    Good_job

--- arkusz12/zapisz.php
+++ arkusz12/zapisz.php
@@ -2,14 +2,18 @@
 $ip = 'localhost';
 $user = 'root';
 $password = '';
-$database  = 'db20'; //laptop ;)
-$imie = $_GET['imie'];
-$nazwisko = $_GET['nazwisko'];
-$adres = $_GET['adres'];
+//$database  = 'db20'; //laptop ;)
+$database = 'db16'; //pc
+$conn = mysqli_connect($ip, $user, $password, $database);
+$imie = $_POST['imie'];
+$nazwisko = $_POST['nazwisko'];
+$adres = $_POST['adres'];
 //zapytanie1 
-$query = 'INSERT INTO karty_wedkarskie 
-(id,imie,nazwisko,adres,data_zezwolenia,punkty)
+$query = "INSERT INTO karty_wedkarskie 
+(id, imie, nazwisko, adres, data_zezwolenia, punkty)
 VALUES
-(null,$imie,$nazwisko,$adres,null,null);';
-mysqli_query($conn,$query)
+(null, '$imie', '$nazwisko', '$adres', null, null);";
+mysqli_query($conn, $query);
+echo "dane zapisane: ". $imie." " .$nazwisko." ". $adres. " ";
+mysqli_close($conn);
 ?>
