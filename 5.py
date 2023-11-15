022977c12dfc042c4f5412665b4403f420d36959
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-11-15 22:11:10 +0100

    G

--- 5.py
+++ 5.py
@@ -1,3 +1,5 @@
 import random # import funkcji random
-moje_opcje = ["DeathStranding","Youtube_kursy","ksiązka","matma","pieniądze"]
+projekty = ["lego", "samochod","przyszlosc","organizacja"]
+random_number = random.randint(0,3)
+moje_opcje = ["DeathStranding","Youtube_kursy","ksiązka","matma","pieniądze",projekty[random_number]]
 print(random.choice(moje_opcje))
