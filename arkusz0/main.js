3df2908530da998a8ab78550bcd7af088d1e6246
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-11-27 15:09:46 +0100

    hj

---
+++ arkusz0/main.js
@@ -0,0 +1,14 @@
+var produkt = document.getElementById('produkt');
+if(produkt === 0){
+    console.log("sigma");
+}
+else {
+    document.write("not sigma");
+}
+
+function aktualizuj(){
+    document.write("lol");
+}
+function zamow(){
+    document.write("lol");
+}
