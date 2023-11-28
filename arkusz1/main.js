189aa0f15d458b767b34fb65ed61aeed0ecd63c3
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-11-28 13:33:44 +0100

    Baba_G

---
+++ arkusz1/main.js
@@ -0,0 +1,7 @@
+var indigo = document.getElementById('indigo').onclick ;
+var steel = document.getElementById('steel').onclick ;
+var olive = document.getElementById('olive').onclick ;
+
+function indigo(){
+    alert('Przycisk wcisniety');
+}
