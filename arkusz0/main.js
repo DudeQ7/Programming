043195a83ac29ffad330ef5bc67bf9a75a83e67e
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-11-28 12:41:33 +0100

    F#ck_this

--- arkusz0/main.js
+++ arkusz0/main.js
@@ -1,14 +1,34 @@
-var produkt = document.getElementById('produkt');
-if(produkt === 0){
-    console.log("sigma");
-}
-else {
-    document.write("not sigma");
+const iloscProduktow = document.querySelectorAll('.ilosc');
+const nazwaProduktow = document.querySelectorAll('.produkt');
+const aktualizuj = document.querySelectorAll('.aktualizuj');
+const zamow = document.querySelectorAll('.zamow');
+var id_zamow = 0;
+
+function sprawdz() {
+    iloscProduktow.forEach((produkt) => {
+        if (produkt.innerHTML == 0) {
+            produkt.style.backgroundColor = 'red';
+        } if(produkt.innerHTML >= 1 && produkt.innerHTML <= 5) {
+            produkt.style.backgroundColor = 'yellow';
+        } if(produkt.innerHTML >= 5) {
+            produkt.style.backgroundColor = 'honeydew';
+        }
+    });
 }
 
-function aktualizuj(){
-    document.write("lol");
-}
-function zamow(){
-    document.write("lol");
-}
+aktualizuj.forEach((przycisk, i) => {
+    przycisk.addEventListener('click', () => {
+        var nowailosc = prompt('Podaj nową ilość: ');
+        iloscProduktow[i].innerHTML = nowailosc;
+        sprawdz();
+    });
+});
+
+zamow.forEach((przycisk, i) => {
+    przycisk.addEventListener('click', () => {
+        alert('Zamówienie nr: ' + id_zamow + ' Produkt: ' + nazwaProduktow[i].innerHTML);
+        id_zamow++;
+    });
+});
+
+sprawdz();
