4c110a2c4ddde6b26d24cae6938cc9b4979e1572
DudeQ7 <dutkiewicz.paw@protonmail.com>
2023-12-10 18:48:19 +0100

    Excercise

---
+++ excersise0/index.js
@@ -0,0 +1,17 @@
+console.log("Pizza");
+console.log("I want to eat a pizza!");
+// window.alert("BUY ME PIZZA!")
+var age ;
+var age = 10 ;
+var fistName  = "John";
+var Lastname = "Doe";
+console.log("Age: " + age);
+age = age + 1 ; //good job :) 
+console.log("birthday: " + age);
+console.log("fistName: " + fistName+ "  "+ "lastname: " + Lastname);
+document.getElementById("p1").innerHTML = "Hello" + " " +fistName ;
+document.getElementById("p2").innerHTML = "are you here? " + Lastname ;
+//comment
+/*comment
+wow
+*/
