1f38e19dd34e1e0d4baa95eb0562c5b30e8b52c8
DudeQ7 <dutkiewicz.paw@protonmail.com>
2024-02-21 18:37:48 +0100

    Good JOB :)

--- Zad2/apple.java
+++ Zad2/apple.java
@@ -1,16 +1,10 @@
 package Zad2;
-
+import Zad2.fruit;
 public class apple extends fruit {
-    private int waga;
-    private String nazwa;
-
-    public apple(int waga, String nazwa) {
-        this.waga = waga;
-        this.nazwa = nazwa;
+    public apple(String typ, double weight) {
+        super(typ, weight);
     }
     public static void main(String[] args) {
-        apple apple1 = new apple(150, "jablko");
-        apple1.nazwa = "lol";
-        System.out.println(apple1.nazwa);
+
     }
 }
--- Zad2/fruit.java
+++ Zad2/fruit.java
@@ -1,4 +1,20 @@
 package Zad2;
 
+import java.lang.reflect.Type;
+
 public class fruit {
+    public fruit(String typ, double weight) {
+        this.typ = typ;
+        this.weight = weight;
+    }
+    public static final String species_Type = "jabłkowate";
+public void getInfo(double weight,String typ){
+    System.out.println("waga: " + weight);
+    System.out.println("Typ:" + typ);
+    }
+    public String typ;
+    public double weight;
+
+    public  String species;
+
 }
