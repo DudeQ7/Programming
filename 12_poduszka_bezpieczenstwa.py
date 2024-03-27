0304b542f6b8c20aa60713d407753359fa9694b9
DudeQ7 <dutkiewicz.paw@protonmail.com>
2024-03-27 08:39:51 +0100

    Ready

--- 12_poduszka_bezpieczenstwa.py
+++ 12_poduszka_bezpieczenstwa.py
@@ -1,11 +1,11 @@
 wydatki1 = 1591.46 #sierpien 
 wydatki2 = 1683.92 #wrzesien
 wydatki3 = 2332.81 # inny miesiac 
-wydatki4 = 1500 
-wydatki4 = 1500
-wydatki5 = 1500
-wydatki6 = 1500
+wydatki4 = 1500  #placeholder
+wydatki4 = 1500 #placeholder
+wydatki5 = 1500 #placeholder
+wydatki6 = 1500 #placeholder
 suma_wydatkow = wydatki1 + wydatki2 +wydatki3 + wydatki4+wydatki5+wydatki6
-print("Konto oszczednosciowe: ",suma_wydatkow * 0.30)
-print("Obligacje EDO: ",suma_wydatkow * 0.70)
-print("Calkowita wartosc poduszki bezpieczenstwa: ", suma_wydatkow)
+print("Konto oszczednosciowe: ",round(suma_wydatkow * 0.30,2))
+print("Obligacje EDO: ",round(suma_wydatkow * 0.70,2))
+print("Calkowita wartosc poduszki bezpieczenstwa: ", round(suma_wydatkow,2))
