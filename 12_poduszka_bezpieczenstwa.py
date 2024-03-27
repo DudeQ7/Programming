8eab5cbf741e749bc72b8e9c486ed3054cc56407
DudeQ7 <dutkiewicz.paw@protonmail.com>
2024-03-27 08:37:32 +0100

    Great JOB

---
+++ 12_poduszka_bezpieczenstwa.py
@@ -0,0 +1,11 @@
+wydatki1 = 1591.46 #sierpien 
+wydatki2 = 1683.92 #wrzesien
+wydatki3 = 2332.81 # inny miesiac 
+wydatki4 = 1500 
+wydatki4 = 1500
+wydatki5 = 1500
+wydatki6 = 1500
+suma_wydatkow = wydatki1 + wydatki2 +wydatki3 + wydatki4+wydatki5+wydatki6
+print("Konto oszczednosciowe: ",suma_wydatkow * 0.30)
+print("Obligacje EDO: ",suma_wydatkow * 0.70)
+print("Calkowita wartosc poduszki bezpieczenstwa: ", suma_wydatkow)
