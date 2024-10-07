e0f9bddb8b9ad05b90b40f5668f35c2b46a76ca7
DudeQ7 <dutkiewicz.paw@protonmail.com>
2024-10-07 08:36:04 +0200

    f

---
+++ Brocode/4.py
@@ -0,0 +1,4 @@
+zmienna = "klotek"
+#print(len(zmienna)) #wypisuje długość danego stringu 
+#print(zmienna.find("e")) #pozwala znaleźć na którym indeksie w naszej zmiennej znajduje sie szukana rzecz zaczuna od 0 
+#print(zmienna.capitalize()) #zwieksza na duza literke
