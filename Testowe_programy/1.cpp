a76d0b9ca0d1dcc0e7f7309ff135c2f5c7d9df63
DudeQ7 <dutkiewicz.paw@protonmail.com>
2024-10-14 14:43:08 +0200

    all good

---
+++ Testowe_programy/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int liczba_1 = 5;
+    int liczba_2 = 10; 
+    int liczba_3 = 15;
+    int zmienna = (liczba_1  + liczba_2 + liczba_3) /3 ;
+
+    cout<< "Srednia wynosi: " ; cout << zmienna << endl;
+    return 0; 
+}
