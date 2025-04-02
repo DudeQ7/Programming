9045d0bb452c5d2c73c8c5118a8cf7f3e1872a1c
DudeQ <dutkiewicz.paw@protonmail.com>
2025-04-02 10:17:23 +0200

    okay

--- L_01/zadania/z1.cpp
+++ L_01/zadania/z1.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <stdio.h>
 /* Niech będzie dana zmienna i typu int. Napisz wstawkę asemblerową przypisującą zmiennej i wartość 10.
-Użyj rejestru eax */ 
+Użyj rejestru eax */
+
 int main()
 {
-int i = 10;
-__asm add eax, i;
-std::cout<<add eax, i;
-return 0; 
+	int i = 10;
+	__asm
+	{
+		mov eax, 10;
+		mov i, eax;
+	}
+	std::cout <<i;
+	return 0;
 }
---
+++ L_01/zadania/z2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/*
+ Niech i, j, k będą zmiennymi typu int. Napisz wstawkę asemblerową wykonującą operację k=i+j. Użyj
+rejestrów eax i ebx
+*/
+int main()
+{
+    int i,j,k;
+    __asm eax
+    return 0;
+}
