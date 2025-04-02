94230ecb559ba59aa1b1380aeb0436a538fd440b
DudeQ <dutkiewicz.paw@protonmail.com>
2025-04-03 00:22:13 +0200

    Yupeee :3

--- L_01/zadania/z2.cpp
+++ L_01/zadania/z2.cpp
@@ -5,7 +5,17 @@ rejestrów eax i ebx
 */
 int main()
 {
-    int i,j,k;
-    __asm eax
+    int i, j, k;
+    __asm
+    {
+        mov i, 15;
+        mov j, 10;
+        mov eax, i;
+        mov ebx, j;
+        add eax, ebx;
+        mov k, eax;
+      
+    }
+    std::cout << k;
     return 0;
 }
