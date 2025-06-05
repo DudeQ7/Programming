1630cec30d44e99bb4867ffe139f1695b2cfd190
DudeQ <dutkiewicz.paw@protonmail.com>
2025-06-05 11:17:37 +0200

    yupee

---
+++ SO_L08/zadanie/zad1[uno]/zad1_nadaj.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/ipc.h> //not sure about it 
+#include <sys/types.h> //not sure about it either 
+#include <sys/mman.h>
+//ma wpisac do shared memory "pamięć współdzielona 123"
+/*
+shmget(), 
+shmat(), 
+shmdt(), 
+shmctl(), 
+ftok(), 
+fork(), 
+wait(), 
+Struktura shmid_ds{} i jej struktury składowe. 
+*/
+int main()
+{
+    shmget()
+    return 0;
+}
---
+++ SO_L08/zadanie/zad1[uno]/zad1_odbierz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//ma odczytac zawrtosc pamiec wspolnej i wypisac ja na ekran
+/*
+hmget(), 
+shmat(), 
+shmdt(), 
+shmctl(), 
+ftok(), 
+fork(), 
+wait(), 
+Struktura shmid_ds{} i jej struktury składowe. 
+*/
+int main()
+{
+    //f f
+    printf("Test");
+}
