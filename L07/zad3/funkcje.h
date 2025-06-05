0e0aaee3c41746723d9388e150f5b3bf06cfc40a
DudeQ <dutkiewicz.paw@protonmail.com>
2025-06-05 12:37:41 +0200

    final_one

--- L07/zad3/funkcje.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNKCJE_H
-#define FUNKCJE_H
-//https://www.tutorialspoint.com/cprogramming/c_header_files.htm
-void print_args(char *argv[],int start,int end);
-int find_max(char *argv[],int start,int end);
-int find_min(char *argv[],int start, int end);
-#endif
