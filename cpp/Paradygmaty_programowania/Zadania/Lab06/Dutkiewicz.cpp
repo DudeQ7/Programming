8b0d1cd97efa7c66dbd0cc4f40be1db2467a1494
DudeQ <dutkiewicz.paw@tutamail.com>
2026-04-30 23:28:43 +0200

    skonczony_majewski_l05_zaczete_l06

---
+++ cpp/Paradygmaty_programowania/Zadania/Lab06/Dutkiewicz.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+/*
+Kod (projekt) w pliku tekstowym + sprawozdanie.
+• Kod i sprawozdanie proszę nazwać swoim nazwiskiem.
+• Prześlij pliki w niespakowanej formie.
+*/
+int main()
+{
+    return 0;
+}
