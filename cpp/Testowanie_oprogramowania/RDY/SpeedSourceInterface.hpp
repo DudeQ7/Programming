327d4f71c8859274e55d6c60c5760913087ef396
DudeQ <dutkiewicz.paw@tutamail.com>
2026-05-11 22:19:13 +0200

    Finished Final_version of linux based Testowanie Oprogramowania with CMake, this iteration will only be improved further, there won't be another new reedition

---
+++ cpp/Testowanie_oprogramowania/RDY/SpeedSourceInterface.hpp
@@ -0,0 +1,8 @@
+#pragma once 
+class SpeedSourceInterface 
+{
+    public:
+        virtual ~SpeedSourceInterface() = default;
+        virtual void setSpeed(double newSpeed) = 0;
+        virtual double getSpeed() = 0;
+};
---
+++ cpp/Testowanie_oprogramowania/RDY/test.cpp
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include <cmath>
+#include <iostream>
+#include <thread>
+#include "SpeedFormatter.hpp"
+#include "MockUnits.hpp"
+#include "MockSpeedSource.hpp"
+#include "MockImmediateSpeedSource.hpp"
+#include "TestValues.hpp"
+
+//R1 test
+TEST(SpeedFormatterTest,ShouldRoundSpeedCorrectly_R1)
+{
+    MockUnits units(SpeedUnit::KPH);
+    MockImmediateSpeedSource source;
+    source.setSpeed(10.4);
+    SpeedFormatter formatter(&source, &units, 1);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 10u);
+    source.setSpeed(10.5);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 11u);
+}
+//R2 test
+TEST(SpeedFormatterTest,ShouldUseCurrentUnit_R2)
+{
+    MockUnits units(SpeedUnit::KPH);
+    MockSpeedSource source;
+    source.setSpeed(TV_speed_initial);
+    SpeedFormatter formatter(&source, &units, 1);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 50u);
+    units.setSpeedUnit(SpeedUnit::MPH);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 31u);
+}
+//R3 small change wait 1 second
+TEST(SpeedSourceTest,ShouldHandleSmallChangesWithDelay_R3)
+{
+    MockUnits units(SpeedUnit::KPH);
+    MockSpeedSource source;
+    source.setSpeed(TV_speed_initial);
+    SpeedFormatter formatter(&source, &units, 1);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 50u);
+    source.setSpeed(TV_speed_small_change);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 50u);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    EXPECT_EQ(formatter.getFormattedSpeed(), 52u);
+}
+//R3 big change immediate
+TEST(SpeedSourceTest,ShouldUpdateImmedaitelyForBingChanges_R3)
+{
+    MockUnits units(SpeedUnit::KPH);
+    MockSpeedSource source;
+    source.setSpeed(TV_speed_initial);
+    SpeedFormatter formatter(&source, &units, 1);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 50u);
+    source.setSpeed(TV_speed_big_change);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 53u);
+}
