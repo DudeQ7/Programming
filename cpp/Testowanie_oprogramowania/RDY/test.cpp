082064907ffda363a5fbc108e039aea915f9ffdd
DudeQ <dutkiewicz.paw@tutamail.com>
2026-05-12 09:50:21 +0200

    Added the Branch coverage ,test coverage to the code itself , made multiple different changes and improvements including corrected gitignore, one more thing left to before sending for review

--- cpp/Testowanie_oprogramowania/RDY/test.cpp
+++ cpp/Testowanie_oprogramowania/RDY/test.cpp
@@ -25,9 +25,9 @@ TEST(SpeedFormatterTest,ShouldUseCurrentUnit_R2)
     MockSpeedSource source;
     source.setSpeed(TV_speed_initial);
     SpeedFormatter formatter(&source, &units, 1);
-    EXPECT_EQ(formatter.getFormattedSpeed(), 50u);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 54u);
     units.setSpeedUnit(SpeedUnit::MPH);
-    EXPECT_EQ(formatter.getFormattedSpeed(), 31u);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 34u);
 }
 //R3 small change wait 1 second
 TEST(SpeedSourceTest,ShouldHandleSmallChangesWithDelay_R3)
@@ -36,9 +36,9 @@ TEST(SpeedSourceTest,ShouldHandleSmallChangesWithDelay_R3)
     MockSpeedSource source;
     source.setSpeed(TV_speed_initial);
     SpeedFormatter formatter(&source, &units, 1);
-    EXPECT_EQ(formatter.getFormattedSpeed(), 50u);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 54u);
     source.setSpeed(TV_speed_small_change);
-    EXPECT_EQ(formatter.getFormattedSpeed(), 50u);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 52u);
     std::this_thread::sleep_for(std::chrono::seconds(1));
     EXPECT_EQ(formatter.getFormattedSpeed(), 52u);
 }
@@ -49,7 +49,7 @@ TEST(SpeedSourceTest,ShouldUpdateImmedaitelyForBingChanges_R3)
     MockSpeedSource source;
     source.setSpeed(TV_speed_initial);
     SpeedFormatter formatter(&source, &units, 1);
-    EXPECT_EQ(formatter.getFormattedSpeed(), 50u);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 54u);
     source.setSpeed(TV_speed_big_change);
-    EXPECT_EQ(formatter.getFormattedSpeed(), 53u);
+    EXPECT_EQ(formatter.getFormattedSpeed(), 54u);
 }
