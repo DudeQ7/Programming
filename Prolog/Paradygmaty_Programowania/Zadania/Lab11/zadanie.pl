b05abe6556307a7bb074199f1637b6a60e11880c
DudeQ <dutkiewicz.paw@tutamail.com>
2026-05-14 00:00:23 +0200

    Started Zadanie from Lab11 from paradygmaty programowania , lab07 from przetwarzanie obrazow finally runs as expected

---
+++ Prolog/Paradygmaty_Programowania/Zadania/Lab11/zadanie.pl
@@ -0,0 +1,4 @@
+mezczyzna(Dariusz).mezczyzna(Pawel).mezczyzna(Lukasz).
+kobieta(Anna).kobieta(Asia).kobieta(Karolina).
+%rodzicielstwo (rodzic(rodzic,dziecko)
+rodzic(Dariusz,Anna).
--- Python/przetwarzanieobrazow/Projekt2[Lab07-Lab12]/Kod_1_Lab07/main.py
+++ Python/przetwarzanieobrazow/Projekt2[Lab07-Lab12]/Kod_1_Lab07/main.py
@@ -1,1 +1,7 @@
-print("Test")
+import cv2 as cv  
+import os
+dir_path  = '/mnt/shared/Programming/Python/przetwarzanieobrazow/Projekt2[Lab07-Lab12]/Kod_1_Lab07'
+os.chdir(dir_path)
+photo_1 = cv.imread('test.jpg')
+cv.imshow("zdjecie", photo_1)
+cv.waitKey(0)
