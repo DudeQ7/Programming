a014199064d27ff25afd170ff768581340cb1dcc
DudeQ <dutkiewicz.paw@tutamail.com>
2026-05-29 20:25:17 +0200

    Start of the Lab12 for Paradygmaty Programowania

---
+++ Prolog/Paradygmaty_Programowania/Zadania/Lab12/kod.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    //nwd && suma
+    int a,b;
+    std::cout << "Podaj liczbe numer_1: ";
+    std::cin >> a;
+    std::cout << "Podaj liczbe numer_2: ";
+    std::cin >> b;
+    std::cout <<"Suma to: "<<a+b;
+    return 0;
+}
---
+++ Prolog/Paradygmaty_Programowania/Zadania/Lab12/kod.pl
@@ -0,0 +1,10 @@
+silnia(N,Wynik).
+suma(N,Wynik).
+potega(X,Y,Wynik).
+%gdzie Wynik = Y^X
+liczba_cyfr(N,L).
+czy_potega_dwojki(N).
+czy_pierwsza(N).
+%dowolne:
+nwd(A,B,W).
+maksimum(A,B,M).
