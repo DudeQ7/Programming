d1a33cd85f72ab41ca8bf1f30fca51bb00d5df7a
DudeQ <dutkiewicz.paw@tutamail.com>
2026-05-29 21:12:46 +0200

    Prolog Sum Done

--- Prolog/Paradygmaty_Programowania/Zadania/Lab12/kod.pl
+++ Prolog/Paradygmaty_Programowania/Zadania/Lab12/kod.pl
@@ -4,8 +4,14 @@ silnia(N,Wynik) :-
     N1 is N - 1,
     silnia(N1,W1),
     Wynik is  N * W1.
+%suma liczb od 0 do N 
+suma(0,0).
+suma(N,Wynik) :-
+    N > 0,
+    N1 is N -1,
+    suma(N1,PrevSuma),
+    Wynik is N + PrevSuma.
 
-suma(N,Wynik).
 potega(X,Y,Wynik).
 %gdzie Wynik = Y^X
 liczba_cyfr(N,L).
