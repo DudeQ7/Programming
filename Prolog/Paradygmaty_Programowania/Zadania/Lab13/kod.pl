05b11907ab50a567e42da056eaedeb996e7c50cc
DudeQ <dutkiewicz.paw@tutamail.com>
2026-05-30 15:49:48 +0200

    Prolog Average Points Done

--- Prolog/Paradygmaty_Programowania/Zadania/Lab13/kod.pl
+++ Prolog/Paradygmaty_Programowania/Zadania/Lab13/kod.pl
@@ -12,7 +12,14 @@ suma_punktow([],0). %pusta lista = 0 pkt
 suma_punktow([student(_, Punkty) | T], Suma) :-
     suma_punktow(T, SumaOgona),
     Suma is Punkty + SumaOgona.
+
 liczba_studentow([],0).
 liczba_studentow([_|T],N) :-
     liczba_studentow(T,N1),
     N is N1 + 1. 
+    
+srednia_punktow(Lista,Srednia) :-
+    suma_punktow(Lista,S),
+    liczba_studentow(Lista,N),
+    N > 0, %failsafe przed dzieleniem przez 0 
+    Srednia is S / N. 
