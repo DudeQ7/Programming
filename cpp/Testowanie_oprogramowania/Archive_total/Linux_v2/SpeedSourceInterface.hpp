279c8dcfaab7cca1e0fef96ca72296d62b2eb569
DudeQ <dutkiewicz.paw@tutamail.com>
2026-06-02 10:08:06 +0200

    Cleanup -Deleted all th previous unused iterations from Testowanie Oprogramowania

--- cpp/Testowanie_oprogramowania/Archive_total/Linux_v2/SpeedSourceInterface.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-class SpeedSourceInterface {
-public:
-    virtual ~SpeedSourceInterface() = default;
-    virtual void setSpeed(double newSpeed) = 0; 
-    virtual double getSpeed() = 0;
-};
--- cpp/Testowanie_oprogramowania/Linux_v2/TestValues.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-extern const double TV_speed_initial;
-extern const double TV_speed_small_change;
-extern const double TV_speed_big_change;
-extern const double TV_units_multiplier_kph;
-extern const double TV_units_multiplier_mph;
