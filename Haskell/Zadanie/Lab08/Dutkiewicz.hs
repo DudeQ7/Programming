c73559341ee885690786eaa894e3e70572f31f93
DudeQ <dutkiewicz.paw@tutamail.com>
2026-06-07 20:10:41 +0200

    Dziencase, parzysta Haskell Done

--- Haskell/Zadanie/Lab08/Dutkiewicz.hs
+++ Haskell/Zadanie/Lab08/Dutkiewicz.hs
@@ -1,4 +1,4 @@
--- Sprawdza podana liczbe i wraca odpowiedni dzien tygodnia 
+-- Sprawdza podana liczbe i zwraca odpowiedni dzien tygodnia 
 dzienif :: Int -> String
 dzienif dzien = 
     if dzien == 1 then "Poniedzialek"
@@ -9,3 +9,18 @@ dzienif dzien =
     else if dzien == 6 then "Sobota"
     else if dzien == 7 then "Niedziela"
     else "Nie ma takiego dnia tygodnia"
+
+dziencase :: Int -> String
+dziencase dzien = case dzien of --deklaracja case'a 
+    1 -> "Poniedzialek"
+    2 -> "Wtorek"
+    3 -> "Sroda"
+    4 -> "Czwartek"
+    5 -> "Piatek"
+    6 -> "Sobota"
+    7 -> "Niedziela"
+    _ -> "Nie ma takiego dnia tygodnia"
+-- Funkcja parzysta 
+parzysta :: Int -> String 
+parzysta n = if even n then "Parzysta" else "Nieparzysta"
+
