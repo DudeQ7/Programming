b1f74bf22b53ef3d5eecdf8b8511873b97b610bf
DudeQ <dutkiewicz.paw@tutamail.com>
2026-06-08 09:24:00 +0200

    Haskell Finished Zadanie 3

--- Haskell/Cwiczenie/Cw.2/cwiczenia_listy_krotki.hs
+++ Haskell/Cwiczenie/Cw.2/cwiczenia_listy_krotki.hs
@@ -1,11 +1,16 @@
+--Done
 temperatury :: [Double]
 temperatury = [18.5,21.0,19.8,23.4,17.9,22.2,20.1]
+
 produkty :: [(String, Double)]
 produkty = [("monitor",899.98),("klawiatura",149.90),("mysz",79.90),("router",249.00)]
+
 punktyLab :: [(String,[Int])]
 punktyLab = [("Nowak",[8,9,7]),("Kowalska",[10,10,9]),("Lis",[5,6,4])]
+
 xsy :: [Int]
 xsy = [2,4,6,8]
+
 ysy :: [Int]
 ysy = [1,3,5,7]
 
@@ -26,11 +31,21 @@ maxMin = (maximum temperatury, minimum temperatury)
 --Zadanie 2 
 -- Cieple Dni = pozostawia temperatury większe lub równe 20.0 = Done 
 ciepleDni :: [Double] -> [Double]
-ciepleDni = filter (>=20.0) 
+ciepleDni ts = filter (>=20.0) ts
 -- zaokraglone = zamienia temperatury na liczby całkowite funkcją round = Done 
 zaokraglone :: [Double] -> [Int]
-zaokraglone = map round
+zaokraglone ts = map round ts
 --rozniceOd20 = oblicza różnicę każdego pomiaru względem wartości 20.0 = Done 
 rozniceOd20 :: [Double] -> [Double]
-rozniceOd20 = map (\t -> t - 20.0)
+rozniceOd20 ts = map (\t -> t - 20.0) ts
 
+--Zadanie 3 
+--wielokrotnosci3 = zwraca wielokrotności liczby 3 od 3 do podanej granicy = Done 
+wielokrotnosci3 :: Int -> [Int]
+wielokrotnosci3 n = [x | x <- [3..n], x `mod` 3 == 0]
+--kwadratyNieparzystych = zwraca kwadraty liczb nieparzystych z zakresu od 1 do n 
+kwadratyNieparzystych :: Int -> [Int]
+kwadratyNieparzystych n = [x^2 | x <- [1..n], odd x]
+--paryRozne = zwraca kwadraty liczb nieparzystych z zakresu od 1 do n  = Done 
+paryRozne ::[Int] -> [Int] -> [(Int,Int)]
+paryRozne xs ys = [(x,y) | x <- xs, y <- ys, x /= y]
