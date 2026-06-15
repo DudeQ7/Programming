5531941444f3f9749bf7ed0f6afcd6489449ca71
DudeQ <dutkiewicz.paw@tutamail.com>
2026-06-15 09:12:30 +0200

    Haskell Excercise Suma na Liczbach + Elementy Spelniajace warunek

---
+++ Haskell/Cwiczenie/Cw.3/cw1.hs
@@ -0,0 +1,11 @@
+-- suma na liczbach
+zad1::Int -> Int 
+zad1 n 
+    | n <=0 = 0
+    | otherwise  = n*n + zad1 (n-1)
+-- liczenie elemnentow spelniajacych warunek
+zad2::[Int] -> Int
+zad2 [] = 0 
+zad2 (x:xs)
+    | x > 0 = 1 + zad2 xs 
+    | otherwise = zad2 xs
