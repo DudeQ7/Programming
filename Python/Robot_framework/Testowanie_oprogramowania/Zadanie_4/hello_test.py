781e847d70de88fbbbfaaa28ee6035d2b33af7f4
DudeQ <dutkiewicz.paw@tutamail.com>
2026-06-15 08:40:19 +0200

    Android Testing Reformatted the code

--- Python/Robot_framework/Testowanie_oprogramowania/Zadanie_4/hello_test.py
+++ Python/Robot_framework/Testowanie_oprogramowania/Zadanie_4/hello_test.py
@@ -3,7 +3,6 @@ import time
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
-
 capabilities = dict(
     platformName='Android',
     automationName='uiautomator2',
@@ -13,30 +12,24 @@ capabilities = dict(
     language='en',
     locale='US'
 )
-
 appium_server_url = 'http://localhost:4723'
-
 class TestAppium(unittest.TestCase):
     def setUp(self) -> None:
         options = UiAutomator2Options().load_capabilities(capabilities)
         self.driver = webdriver.Remote(appium_server_url, options=options)
-
     def tearDown(self) -> None:
         if self.driver:
             self.driver.quit()
-
     def test_find_battery(self) -> None:
         # el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
         el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Sounds and vibration"]')
         el.click()
         el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Call vibration"]')
         el.click()
-
         sounds = ['Heartbeat', 'Ticktock', 'Waltz', 'Siren', 'Basic call']
         for sound in sounds:
             el = self.driver.find_element(by=AppiumBy.XPATH, value=f'//*[@text="{sound}"]')
             el.click()
             time.sleep(3)
-
 if __name__ == '__main__':
     unittest.main()
