0fe4cf6d809fae1c1b8f78361829910f01e4226c
DudeQ <dutkiewicz.paw@tutamail.com>
2026-06-15 08:39:58 +0200

    Haskell Added new Excercises

--- Haskell/Cwiczenie/Cw.3/cw.hs
+++ Haskell/Cwiczenie/Cw.3/cw.hs
@@ -7,3 +7,13 @@ funkcja1 x=(a* (sin b) / (b*b*b - 6*a))
 silnia:: Integer -> Integer
 silnia 0 = 1 
 silnia n = n*silnia(n-1)
+
+silnia2:: Integer -> Maybe Integer
+silnia2 n 
+    | n < 0 =Nothing
+    | n==0 = Just 1
+    | otherwise = Just (n * fromJust (silnia2 (n-1)))
+
+potega:: Integer -> Integer -> Integer
+    potega a 0 = 1
+    potega a b = a * potega a (b-1)
